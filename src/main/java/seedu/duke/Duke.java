fd16d26f86c70f6207544eafb45f4de86c523b96
flyingapricot <126645544+flyingapricot@users.noreply.github.com>
2025-02-25 14:44:35 +0800

    Update Duke.java

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public final class Duke {
     // Private constructor to prevent instantiation
     private Duke() {
-        throw new UnsupportedOperationException("Duke is a utility \
-                                                class and cannot be instantiated.");
+        throw new UnsupportedOperationException(
+            "Duke is a utility" +
+            "class and cannot be instantiated.");
     }
     /**
      * Main entry-point for the java.duke.Duke application.
