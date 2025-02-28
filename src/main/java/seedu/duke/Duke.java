ba186cbf8c70f5a270f18c826568c626d45056e9
flyingapricot <126645544+flyingapricot@users.noreply.github.com>
2025-02-28 21:49:57 +0800

    Update Duke.java

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -2,25 +2,20 @@ package seedu.duke;
 
 import java.util.Scanner;
 
-public final class Duke {
-  // Private constructor to prevent instantiation
-  /**
-   * Main entry-point for the java.duke.Duke application.
-   *
-   * @param args The command-line arguments passed to the program.
-   */
-  public static void main(final String[] args) {
-    String logo =
-        " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
-    System.out.println("Hello from\n" + logo);
-    System.out.println("What is your name?");
-    int valuetest = 2;
+public class Duke {
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
 
-    Scanner in = new Scanner(System.in);
-    System.out.println("Hello " + in.nextLine());
-  }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
+    }
 }
